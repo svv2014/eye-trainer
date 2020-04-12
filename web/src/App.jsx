ecce78544bf45a90bad440fc4330d63ec03a2a15
Vadym Suprun <vadim.svv@gmail.com>
2020-04-11 23:07:07 -0400

    some more text to disclaimer

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -6,8 +6,8 @@ const App = () => (
   <div className="App">
       <h1 className={"programName"}>Eyes Trainer</h1>
       <h3>Disclaimer: Please check with your Eye Doctor fist before starting exercises.
-          Contact lenses cannot be worn before, during, after doing exercises. </h3>
-
+          Contact lenses cannot be worn before, during, after doing exercises.
+          By continuing you agree to use this website at your own risk.</h3>
       <br/>
       <h2>Start exercising by choosing difficulty </h2>
       <h3>Beginners should start with Easy level once a day during one week. After then moderate as you feel.
